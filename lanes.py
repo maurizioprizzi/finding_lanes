f3dfe6ccff44495e595a8f0cd4342d558a7f260d
Maurizio Prizzi <maurizioprizzi@gmail.com>
2024-08-04 13:19:27 -0300

    Primeiro passo no projeto, importando a biblioteca OpenCV e mostrando a imagem

---
+++ lanes.py
@@ -0,0 +1,13 @@
+# importando a biblioteca OpenCV
+import cv2
+import numpy as np
+
+# return the image as a multidimensional numpy array
+image = cv2.imread('test_image.jpg')
+lane_image = np.copy(image)
+gray = cv2.cvtColor(lane_image, cv2.COLOR_BGR2GRAY)
+
+cv2.imshow('result', gray)
+cv2.waitKey(0)
+
+# 
