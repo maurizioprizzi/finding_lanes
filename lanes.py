6c7db621c3f584f8e69f71e2ee8143f78223fd06
Maurizio Prizzi <maurizioprizzi@gmail.com>
2024-08-04 14:15:07 -0300

    Applying the GaussanBlur to the image with a kernel of  5 by 5 and a deviation of 0

--- lanes.py
+++ lanes.py
@@ -7,10 +7,10 @@ image = cv2.imread('test_image.jpg')
 
 # make a copy of the image
 lane_image = np.copy(image)
-
+# make a copy of the gray scale image
 gray = cv2.cvtColor(lane_image, cv2.COLOR_BGR2GRAY)
+# apply GaussianBlur filter to the image to reduce noise
+blur = cv2.GaussianBlur(gray, (5, 5), 0) # 5*5 kernel, deviation of 0
 
-cv2.imshow('result', gray)
+cv2.imshow('result', blur)
 cv2.waitKey(0)
-
-# 
