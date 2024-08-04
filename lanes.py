e0e24d6368360739aef2b25b2da048072d8d2aff
Maurizio Prizzi <maurizioprizzi@gmail.com>
2024-08-04 13:27:26 -0300

    Segundo passo: fazer copya do array original e transformar a imagem em grayscale

--- lanes.py
+++ lanes.py
@@ -4,7 +4,10 @@ import numpy as np
 
 # return the image as a multidimensional numpy array
 image = cv2.imread('test_image.jpg')
+
+# make a copy of the image
 lane_image = np.copy(image)
+
 gray = cv2.cvtColor(lane_image, cv2.COLOR_BGR2GRAY)
 
 cv2.imshow('result', gray)
