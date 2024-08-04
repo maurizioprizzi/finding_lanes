bd2aac2582fbee0031d62f9f0e73b1c8adc47d7c
Maurizio Prizzi <maurizioprizzi@gmail.com>
2024-08-04 14:25:51 -0300

    Used the canny function on the blur image version

--- lanes.py
+++ lanes.py
@@ -7,10 +7,15 @@ image = cv2.imread('test_image.jpg')
 
 # make a copy of the image
 lane_image = np.copy(image)
+
 # make a copy of the gray scale image
 gray = cv2.cvtColor(lane_image, cv2.COLOR_BGR2GRAY)
+
 # apply GaussianBlur filter to the image to reduce noise
 blur = cv2.GaussianBlur(gray, (5, 5), 0) # 5*5 kernel, deviation of 0
 
-cv2.imshow('result', blur)
+# apply Canny method
+canny = cv2.Canny(blur, 50, 150)
+
+cv2.imshow('result', canny)
 cv2.waitKey(0)
